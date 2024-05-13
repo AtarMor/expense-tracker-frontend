1e178fc5cb3d7e1fe25aed51cedfc3bc061439b3
Atar Mor <atarmor92@gmail.com>
2024-05-13 10:45:38 +0300

    Init-repo

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <main>
+      <h1>Expense Tracker</h1>
+    </main>
+  )
+}
+
+export default App
